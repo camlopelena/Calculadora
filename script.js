34a73ea98b77f2c304523e7e21a085376b516015
camlopelena <camlopelena@gmail.com>
2026-09-03 06:02:58 -0600

    Refactor calculator logic to include nuevoNumero
    Added nuevoNumero flag to handle new input after an operation.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let num1 = 0;
 let num2 = 0;
 let opcion = 0;
+let nuevoNumero = false;
 
 let pantalla = document.querySelector(".pantalla");
 let botones = document.querySelectorAll(".btn");
@@ -23,25 +24,25 @@ for (let i = 0; i < botones.length; i++) {
         else if (boton == "+") {
             num1 = Number(pantalla.textContent);
             opcion = 1;
-            pantalla.textContent = "0";
+            nuevoNumero = true;
         }
 
         else if (boton == "-") {
             num1 = Number(pantalla.textContent);
             opcion = 2;
-            pantalla.textContent = "0";
+            nuevoNumero = true;
         }
 
         else if (boton == "*") {
             num1 = Number(pantalla.textContent);
             opcion = 3;
-            pantalla.textContent = "0";
+            nuevoNumero = true;
         }
 
         else if (boton == "/") {
             num1 = Number(pantalla.textContent);
             opcion = 4;
-            pantalla.textContent = "0";
+            nuevoNumero = true;
         }
 
         else if (boton == "=") {
@@ -57,15 +58,78 @@ for (let i = 0; i < botones.length; i++) {
 
 function numero(n) {
 
-    if (pantalla.textContent == "0") {
+    if (nuevoNumero == true) {
+        pantalla.textContent = n;
+        nuevoNumero = false;
+    }
+
+    else if (pantalla.textContent == "0") {
         pantalla.textContent = n;
     }
+
     else {
         pantalla.textContent = pantalla.textContent + n;
     }
 
 }
 
+
+function operacion() {
+
+    num2 = Number(pantalla.textContent);
+
+    switch(opcion) {
+
+        case 1:
+            let totalSuma = num1 + num2;
+            pantalla.textContent = totalSuma;
+            break;
+
+        case 2:
+            let totalResta = num1 - num2;
+            pantalla.textContent = totalResta;
+            break;
+
+        case 3:
+            let totalMulti = num1 * num2;
+            pantalla.textContent = totalMulti;
+            break;
+
+        case 4:
+            let totalDivi = num1 / num2;
+            pantalla.textContent = totalDivi;
+            break;
+
+        default:
+            alert("Opción no válida");
+            break;
+    }
+
+    nuevoNumero = true;
+}
+
+
+function limpiar() {
+
+    num1 = 0;
+    num2 = 0;
+    opcion = 0;
+    nuevoNumero = false;
+
+    pantalla.textContent = "0";
+}
+
+
+function borrar() {
+
+    if (pantalla.textContent.length > 1) {
+        pantalla.textContent = pantalla.textContent.slice(0, -1);
+    }
+
+    else {
+        pantalla.textContent = "0";
+    }
+}
 
 function operacion() {
 
