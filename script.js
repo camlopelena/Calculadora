e1cdb47586f93ab2f84178b09ece58a7f332624b
camlopelena <camlopelena@gmail.com>
2026-09-03 06:10:47 -0600

    Refactor calculator logic and remove duplicates

--- script.js
+++ script.js
@@ -130,6 +130,73 @@ function borrar() {
         pantalla.textContent = "0";
     }
 }
+    else if (pantalla.textContent == "0") {
+        pantalla.textContent = n;
+    }
+
+    else {
+        pantalla.textContent = pantalla.textContent + n;
+    }
+
+}
+
+
+function operacion() {
+
+    num2 = Number(pantalla.textContent);
+
+    switch(opcion) {
+
+        case 1:
+            let totalSuma = num1 + num2;
+            pantalla.textContent = totalSuma;
+            break;
+
+        case 2:
+            let totalResta = num1 - num2;
+            pantalla.textContent = totalResta;
+            break;
+
+        case 3:
+            let totalMulti = num1 * num2;
+            pantalla.textContent = totalMulti;
+            break;
+
+        case 4:
+            let totalDivi = num1 / num2;
+            pantalla.textContent = totalDivi;
+            break;
+
+        default:
+            alert("Opción no válida");
+            break;
+    }
+
+    nuevoNumero = true;
+}
+
+
+function limpiar() {
+
+    num1 = 0;
+    num2 = 0;
+    opcion = 0;
+    nuevoNumero = false;
+
+    pantalla.textContent = "0";
+}
+
+
+function borrar() {
+
+    if (pantalla.textContent.length > 1) {
+        pantalla.textContent = pantalla.textContent.slice(0, -1);
+    }
+
+    else {
+        pantalla.textContent = "0";
+    }
+}
 
 function operacion() {
 
